7faba4de64420bc769fd881ce30c793164d6ac7f
Brandon Coffey <bmc939@gmail.com>
2023-04-04 09:06:17 -0500

    Setup Single Camera Switching

---
+++ Betrayal Unity Client/Assets/Scripts/Game/MainCameraOption.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraOption : MonoBehaviour
+{
+	private MainCameraSwitcher _switcher;
+	
+	private void Awake()
+	{
+		_switcher = FindObjectOfType<MainCameraSwitcher>();
+	}
+	
+	private void OnEnable()
+	{
+		_switcher.SetCamera(this);
+	}
+	
+	private void OnDisable()
+	{
+		_switcher.RemoveCamera(this);
+	}
+}
---
+++ Betrayal Unity Client/Assets/Scripts/Game/MainCameraSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraSwitcher : MonoBehaviour
+{
+	[SerializeField, ReadOnly] private int _activeIndex = -1;
+	[SerializeField, ReadOnly] private Transform _cameraTransform;
+	[SerializeField, ReadOnly] private List<MainCameraOption> _enabledOptions;
+	
+	public void SetCamera(MainCameraOption option)
+	{
+		_enabledOptions.Add(option);
+		_activeIndex = _enabledOptions.Count - 1;
+		RefreshActive();
+	}
+	
+	public void RemoveCamera(MainCameraOption option)
+	{
+		_enabledOptions.Remove(option);
+		RefreshActive();
+	}
+	
+	private void RefreshActive()
+	{
+		if (_activeIndex >= _enabledOptions.Count)
+		{
+			_activeIndex = _enabledOptions.Count - 1;
+		}
+		_cameraTransform = _activeIndex >= 0 ? _enabledOptions[_activeIndex].transform : null;
+	}
+	
+	private void LateUpdate()
+	{
+		if (_activeIndex >= 0)
+		{
+			transform.SetPositionAndRotation(_cameraTransform.position, _cameraTransform.rotation);
+		}
+	}
+}
