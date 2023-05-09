b96f6cae29c58ecd76d55deba3118091a87632fd
Brandon Coffey <bmc939@gmail.com>
2023-05-08 23:03:10 -0500

    Clean Up Namespaces

--- Betrayal Unity Client/Assets/Scripts/Events/EventBase.cs
+++ Betrayal Unity Client/Assets/Scripts/Events/EventBase.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "Betrayal/Event")]
 public class EventBase : ScriptableObject
