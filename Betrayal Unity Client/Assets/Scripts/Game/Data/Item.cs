f85f3679cee47204c544da5cbf57f395061c482d
Brandon Coffey <bmc939@gmail.com>
2023-05-10 14:52:20 -0500

    Import Art

--- Betrayal Unity Client/Assets/Scripts/Game/Data/Item.cs
+++ Betrayal Unity Client/Assets/Scripts/Game/Data/Item.cs
@@ -13,24 +13,26 @@ public class Item : ScriptableObject
 {
 	[Header("Data")]
 	[SerializeField] private int _id;
-	[SerializeField] private string _name;
 	[SerializeField] private bool _omen;
 	[SerializeField] private ItemType _type;
-
-	[Header("Descriptors")]
-	[SerializeField] private Sprite _icon;
+	[SerializeField] private string _name;
 	[SerializeField, TextArea] private string _description;
+
+	[Header("References")]
+	[SerializeField] private Sprite _icon;
+	[SerializeField] private GameObject _prefab;
 	
 	[Header("Effects")]
 	[SerializeField, TextArea] private string _effectsDescription;
 	
 	public int Id => _id;
 	public void SetId(int id) => _id = id;
-	public string Name => _name;
 	public bool Omen => _omen;
 	public ItemType Type => _type;
+	public string Name => _name;
 	public string Description => _description;
 	public Sprite IconSprite => _icon;
+	public GameObject Prefab => _prefab;
 	public string EffectsDescription => _effectsDescription;
 	
 	private void OnValidate()
