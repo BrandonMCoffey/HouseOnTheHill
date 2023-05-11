90f7ae8fb545e58abac6b1d72dd20fa13a06148b
Brandon Coffey <bmc939@gmail.com>
2023-05-11 13:14:01 -0500

    Major All Room Art Overhaul

--- Betrayal Unity Client/Assets/Scripts/Rooms/RoomDecoration.cs
+++ Betrayal Unity Client/Assets/Scripts/Rooms/RoomDecoration.cs
@@ -1,18 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CoffeyUtils;
+using UnityEngine;
 
 public class RoomDecoration : MonoBehaviour
 {
-	[SerializeField] private Light _roomLight;
-	[SerializeField] private float _basicLightIntensity = 3;
-	[SerializeField] private Color _basicLightColor = Color.white;
-	[SerializeField] private float _omenLightIntensity = 2;
-	[SerializeField] private Color _omenLightColor = Color.green;
-	[SerializeField] private float _range = 10;
-	
-	public void UpdateLights(Room room)
+	[SerializeField] private List<Light> _mainLights;
+	[Header("Intensity")]
+	[SerializeField] private bool _overrideIntensity;
+	[SerializeField, ShowIf("_overrideIntensity")] private float _basicLightIntensity = 3;
+	[SerializeField, ShowIf("_overrideIntensity")] private float _omenLightIntensity = 2;
+	[Header("Color")]
+	[SerializeField] private bool _overrideColor;
+	[SerializeField, ShowIf("_overrideColor")] private Color _basicLightColor = Color.white;
+	[SerializeField, ShowIf("_overrideColor")] private Color _omenLightColor = Color.green;
+	[Header("Range")]
+	[SerializeField] private bool _overrideRange;
+	[SerializeField, ShowIf("_overrideRange")] private float _range = 10;
+
+	public void UpdateLights(Room room) => UpdateLights(room.Omen);
+	[Button]
+	private void UpdateLights(bool omen)
 	{
-		_roomLight.intensity = room.Omen ? _omenLightIntensity : _basicLightIntensity;
-		_roomLight.color = room.Omen ? _omenLightColor : _basicLightColor;
-		_roomLight.range = _range;
+		foreach (Light l in _mainLights.Where(l => l != null))
+		{
+			if (_overrideIntensity) l.intensity = omen ? _omenLightIntensity : _basicLightIntensity;
+			if (_overrideColor) l.color = omen ? _omenLightColor : _basicLightColor;
+			if (_overrideRange) l.range = _range;
+		}
 	}
 }
